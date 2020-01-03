ef05789ad6daa5fd4a4686d9016b5a6b0c1c5dbf
Yves Hoppe <yves@empoche.com>
2020-01-03 21:59:53 +0100

    [Win] Remove atlstr.h dependency

--- cppsrc/windows/windowwindows.cpp
+++ cppsrc/windows/windowwindows.cpp
@@ -1,27 +1,26 @@
 #include "windowwindows.h"
 
 #include <windows.h>
-#include <atlstr.h>
+#include <stringapiset.h>
 #include <string>
+
 
 void windowwindows::getActiveWindow(Napi::Object& obj) {
 	char window_title[256];
-
 	HWND foreground_window = GetForegroundWindow();
 	GetWindowText(foreground_window, window_title, sizeof(window_title));
 
 	DWORD pid;
 	GetWindowThreadProcessId(foreground_window, &pid);
-
 	// Process
-	LPWSTR process_filename = new WCHAR[MAX_PATH];
+	TCHAR process_filename[MAX_PATH];
 	DWORD charsCarried = MAX_PATH;
 
 	HANDLE hProc = OpenProcess(PROCESS_QUERY_LIMITED_INFORMATION | PROCESS_QUERY_INFORMATION, false, pid);
 
-	QueryFullProcessImageNameW(hProc, 0, process_filename, &charsCarried);
+	QueryFullProcessImageNameA(hProc, 0, process_filename, &charsCarried);
 
-	std::string fullpath = CW2A(process_filename);
+	std::string fullpath = process_filename;
 
 	const size_t last_slash_idx = fullpath.find_last_of("\\/");
 
@@ -29,13 +28,22 @@ void windowwindows::getActiveWindow(Napi::Object& obj) {
 		fullpath.erase(0, last_slash_idx + 1);
 	}
 
+	// Last input time
+	LASTINPUTINFO last_input;
+    // without setting cbSize GetLastError() returns the parameter is incorrect
+    last_input.cbSize = sizeof(last_input);  
+	DWORD idle_time;
+
+	GetLastInputInfo( &last_input );
+	idle_time = (GetTickCount() - last_input.dwTime) / 1000;
+
 	obj.Set("os", "windows");
 	obj.Set("windowClass", fullpath);
 	obj.Set("windowName", window_title);
 	obj.Set("windowDesktop", "0");
 	obj.Set("windowType", "0");
 	obj.Set("windowPid", std::to_string(pid));
-	obj.Set("idleTime", "0");
+	obj.Set("idleTime", std::to_string(idle_time));
 }
 
 Napi::Object windowwindows::getActiveWindowWrapped(const Napi::CallbackInfo& info)
